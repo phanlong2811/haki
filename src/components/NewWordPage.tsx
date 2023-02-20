cb7474443cb3ced02a9aa6c8cb5857a9257bd4a5
Phan Long <ptlong2811@gmail.com>
2023-02-20 15:21:09 +0700

    Deleted `Completed!` in add word

--- src/components/NewWordPage.tsx
+++ src/components/NewWordPage.tsx
@@ -26,117 +26,111 @@ export default function NewWordPage() {
     });
   }, [searchParams]);
   return (
-    <>
-      <Grid columns={2} stackable>
-        <Grid.Column>
-          <h2 style={{ color: 'black' }}>Information</h2>
-          <Form>
-            <Form.Field>
-              <label>Word</label>
-              <Input
-                placeholder="Word"
-                value={flashCard.word}
-                onChange={(e, data) => {
-                  setFlashCard({
-                    ...flashCard,
-                    word: data.value,
-                  });
-                }}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Word Type</label>
-              <Input
-                placeholder="Word type"
-                value={flashCard.type}
-                onChange={(e, data) => {
-                  setFlashCard({
-                    ...flashCard,
-                    type: data.value,
-                  });
-                }}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Phonetic</label>
-              <Input
-                placeholder="Phonetic"
-                value={flashCard.phonetic}
-                onChange={(e, data) => {
-                  setFlashCard({
-                    ...flashCard,
-                    phonetic: data.value,
-                  });
-                }}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Meaning</label>
-              <Input
-                placeholder="Meaning"
-                value={flashCard.mean}
-                onChange={(e, data) => {
-                  setFlashCard({
-                    ...flashCard,
-                    mean: data.value,
-                  });
-                }}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Image Link</label>
-              <Input
-                placeholder="Image Link"
-                value={flashCard.image}
-                onChange={(e, data) => {
-                  setFlashCard({
-                    ...flashCard,
-                    image: data.value,
-                  });
-                }}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Example</label>
-              <Input
-                placeholder="Example"
-                value={flashCard.example_en}
-                onChange={(e, data) => {
-                  setFlashCard({
-                    ...flashCard,
-                    example_en: data.value,
-                  });
-                }}
-              />
-            </Form.Field>
-            <Button
-              as={Link}
-              type="submit"
-              primary
-              onClick={addWord}
-              to="/browser"
-            >
-              Add
-            </Button>
-          </Form>
-        </Grid.Column>
+    <Grid columns={2} stackable>
+      <Grid.Column>
+        <h2 style={{ color: 'black' }}>Information</h2>
+        <Form>
+          <Form.Field>
+            <label>Word</label>
+            <Input
+              placeholder="Word"
+              value={flashCard.word}
+              onChange={(e, data) => {
+                setFlashCard({
+                  ...flashCard,
+                  word: data.value,
+                });
+              }}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Word Type</label>
+            <Input
+              placeholder="Word type"
+              value={flashCard.type}
+              onChange={(e, data) => {
+                setFlashCard({
+                  ...flashCard,
+                  type: data.value,
+                });
+              }}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Phonetic</label>
+            <Input
+              placeholder="Phonetic"
+              value={flashCard.phonetic}
+              onChange={(e, data) => {
+                setFlashCard({
+                  ...flashCard,
+                  phonetic: data.value,
+                });
+              }}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Meaning</label>
+            <Input
+              placeholder="Meaning"
+              value={flashCard.mean}
+              onChange={(e, data) => {
+                setFlashCard({
+                  ...flashCard,
+                  mean: data.value,
+                });
+              }}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Image Link</label>
+            <Input
+              placeholder="Image Link"
+              value={flashCard.image}
+              onChange={(e, data) => {
+                setFlashCard({
+                  ...flashCard,
+                  image: data.value,
+                });
+              }}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Example</label>
+            <Input
+              placeholder="Example"
+              value={flashCard.example_en}
+              onChange={(e, data) => {
+                setFlashCard({
+                  ...flashCard,
+                  example_en: data.value,
+                });
+              }}
+            />
+          </Form.Field>
+          <Button
+            as={Link}
+            type="submit"
+            primary
+            onClick={addWord}
+            to="/browser"
+          >
+            Add
+          </Button>
+        </Form>
+      </Grid.Column>
 
-        <Grid.Column>
-          <h2 style={{ color: 'black' }}>Preview</h2>
-          <DetailFlashCard
-            word={flashCard.word}
-            type={flashCard.type}
-            mean={flashCard.mean}
-            image={flashCard.image}
-            phonetic={flashCard.phonetic}
-            example_en={flashCard.example_en}
-          />
-        </Grid.Column>
-      </Grid>
-      <Message positive>
-        <Message.Header>Completed!</Message.Header>
-        <p>Now you can see your word in database!</p>
-      </Message>
-    </>
+      <Grid.Column>
+        <h2 style={{ color: 'black' }}>Preview</h2>
+        <DetailFlashCard
+          word={flashCard.word}
+          type={flashCard.type}
+          mean={flashCard.mean}
+          image={flashCard.image}
+          phonetic={flashCard.phonetic}
+          example_en={flashCard.example_en}
+        />
+      </Grid.Column>
+    </Grid>
   );
 }
